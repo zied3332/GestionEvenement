49aa7e1c5f27f034e6902c6da2beb7544e9596b6
zied3332 <141698161+zied3332@users.noreply.github.com>
2026-09-23 23:25:22 +0100

    feat: add Event and Person applications

--- GestionEvenement/settings.py
+++ GestionEvenement/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
 
+    'Event',
+    'Person',
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
