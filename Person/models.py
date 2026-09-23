5143253433249ca3227c124abc45d073cb657bf0
zied3332 <141698161+zied3332@users.noreply.github.com>
2026-09-23 23:29:22 +0100

    feat: add Person model and validation

--- Person/models.py
+++ Person/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+def validate_esprit_email(value):
+    if not value.endswith("@esprit.tn"):
+        raise ValidationError("Email must end with @esprit.tn")
+
+
+class Person(User):
+    cin = models.CharField(
+        max_length=8,
+        primary_key=True,
+        validators=[MinLengthValidator(8)],
+    )
+
+    def clean(self):
+        super().clean()
+        validate_esprit_email(self.email)
